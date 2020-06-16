1746ef67e4b970669824ed39c2b3a89226a43858
OnurMorcagok <onurmorcagok@gmail.com>
2020-06-16 15:53:36 +0300

    Notes added

---
+++ Object Create/app.js
@@ -0,0 +1,43 @@
+// const object = {
+
+//     testFunction : function(){
+//         console.log("Test 1 Function");
+//     },
+
+//     testFunction2 : function() {
+//         console.log("Test 2 Function");
+//     }
+
+// };
+
+// const employee = Object.create(object); // Object nesnesinden employee nesnesi oluştur.
+
+// employee.name = "Onur Morcagok";
+// employee.age = 23;
+// employee.department = "Software Development";
+
+// console.log(employee);
+
+function Person() {
+
+}
+
+Person.prototype.test1 = function () {
+    console.log("Test 1 Function");
+}
+
+Person.prototype.test2 = function () {
+    console.log("Test 2 Function");
+}
+
+function Employee(name, age) {
+
+    this.name = name;
+    this.age = age;
+}
+
+Employee.prototype = Object.create(Person.prototype);
+
+const emp = new Employee("Onur",23);
+
+console.log(emp);
